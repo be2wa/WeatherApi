473c87aa8cc111f1868fbbe9329f1d487c915f87
Bartlomiej Kowalczyk <bart.kowalczyk@gmail.com>
2017-05-11 16:55:08 +0200

    file config

---
+++ js/app.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+require('../scss/style.scss');
+
+document.addEventListener('DOMContentLoaded', function(){
+
+class App extends React.Component {
+    render() {
+        return <div>huj</div>
+    }
+}
+    
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+);
+    
+});
